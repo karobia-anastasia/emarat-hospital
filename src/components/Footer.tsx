d42946df334c1e2917d01427ccb9a5f35071721c
wanjugucode <karobiaanastasia>
2025-10-29 11:53:21 +0300

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Facebook, Twitter, Instagram, Linkedin } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-900 text-secondary-foreground">
+    <footer className="bg-gray-700 text-secondary-foreground">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-8">
           <div>
